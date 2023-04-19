b5a4ba2b861f3f498f5fdd3621ac07b26ef26523
Zheng, Bang-Bo <zhengbangbo@hotmail.com>
2023-04-19 19:33:58 +0800

    chore: update constants

--- constants/index.ts
+++ constants/index.ts
@@ -1,2 +1,2 @@
-export const appName = 'Vitesse for Nuxt 3'
-export const appDescription = 'Vitesse for Nuxt 3'
+export const appName = 'Public Database Charts'
+export const appDescription = 'Visualization for Public Database'
